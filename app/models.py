63b9bc58983dd3e5b0253f4c349a8dba5f93b52b
Tiago Berwanger <tiago.berwanger@mercos.com>
2023-07-30 22:44:50 -0300

    adiciona models de Imovel e Contrato

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
-from .database import Base
-from sqlalchemy import Column, Integer, String, Date, TIMESTAMP
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Date, Enum, DECIMAL, TIMESTAMP
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
+
+from .database import Base
 
 
 class Inquilino(Base):
@@ -14,3 +16,30 @@ class Inquilino(Base):
     estado = Column(String, nullable=True)
     email = Column(String, nullable=True)
     ultima_alteracao = Column(TIMESTAMP(timezone=True), default=None, onupdate=func.now())
+
+
+class Imovel(Base):
+    __tablename__ = "imoveis"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    tipo = Column(Enum, nullable=False)
+    numero = Column(Integer, nullable=False)
+    descricao = Column(String, nullable=True)
+    disponivel = Column(Boolean, default=True, nullable=False)
+    valor = Column(DECIMAL, nullable=True)
+    endereco = Column(String, nullable=True)
+    area = Column(Integer, nullable=True)
+
+
+class Contrato(Base):
+    __tablename__ = "contratos"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    inquilino_id = Column(Integer, ForeignKey("inquilinos.id"), nullable=False)
+    imovel_id = Column(Integer, ForeignKey("imoveis.id"), nullable=False)
+    inquilino = relationship("Inquilino", backref="contratos")
+    imovel = relationship("Imovel", backref="contratos")
+    data_inicio = Column(Date, nullable=False)
+    data_termino = Column(Date, nullable=False)
+    valor_aluguel = Column(DECIMAL, nullable=False)
+    status = Column(Enum, nullable=False)
