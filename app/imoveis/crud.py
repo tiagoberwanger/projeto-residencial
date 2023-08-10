19568c2a8d1f40a893f1d3af50dd35c150952156
Tiago Berwanger <tiago.berwanger@mercos.com>
2023-08-10 09:04:15 -0300

    refatora CRUD de imoveis

--- app/imoveis/crud.py
+++ app/imoveis/crud.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, status, Depends
 from sqlalchemy.orm import Session
 
-import models
+import imoveis.gateway as gateway
 import schemas
 from database import get_db
 
@@ -9,18 +9,33 @@ router = APIRouter()
 
 
 @router.get('/', status_code=status.HTTP_200_OK)
-def obter_imoveis(db: Session = Depends(get_db), limit: int = 10, page: int = 1):
-    skip = (page - 1) * limit
-    imoveis = db.query(models.Imovel).limit(limit).offset(skip).all()
+def obter_imoveis(db: Session = Depends(get_db)):
+    imoveis = gateway.obter_imoveis(db=db)
 
-    return {'results': len(imoveis), 'imoveis': imoveis}
+    return {'quantidade': len(imoveis), 'imovels': imoveis}
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def criar_imovel(payload: schemas.Imovel, db: Session = Depends(get_db)):
-    novo_imovel = models.Imovel(**payload.model_dump())
-    db.add(novo_imovel)
-    db.commit()
-    db.refresh(novo_imovel)
+def criar_imovel(dados: schemas.Imovel, db: Session = Depends(get_db)):
+    return gateway.criar_imovel(dados, db)
 
-    return {"imovel": novo_imovel}
+
+@router.get('/{imovel_id}', status_code=status.HTTP_200_OK)
+def obter_um_imovel(id: int, db: Session = Depends(get_db)):
+    imovel = gateway.obter_um_imovel(id, db)
+
+    return {'imovel': imovel}
+
+
+@router.patch('/{imovel_id}', status_code=status.HTTP_200_OK)
+def editar_um_imovel(id: int, dados: schemas.Imovel, db: Session = Depends(get_db)):
+    imovel_atualizado = gateway.editar_um_imovel(id, dados, db)
+
+    return {'imovel_atualizado': imovel_atualizado}
+
+
+@router.delete('/{imovel_id}', status_code=status.HTTP_204_NO_CONTENT)
+def excluir_um_imovel(id: int, db: Session = Depends(get_db)):
+    imovel_id = gateway.excluir_um_imovel(id, db)
+
+    return {'message': f'Imóvel {imovel_id} excluído'}
