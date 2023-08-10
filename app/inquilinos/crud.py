d2a315e2b49d7cd98ee3f9cd6b80fb19036efb0f
Tiago Berwanger <tiago.berwanger@mercos.com>
2023-08-10 08:39:52 -0300

    ajusta arquitetura do CRUD

--- app/inquilinos/crud.py
+++ app/inquilinos/crud.py
@@ -1,26 +1,41 @@
-from fastapi import APIRouter, status, Depends, HTTPException, Response
+from fastapi import APIRouter, status, Depends
 from sqlalchemy.orm import Session
 
-from app import schemas, models
-from app.database import get_db
+import inquilinos.gateway as gateway
+import schemas
+from database import get_db
 
 router = APIRouter()
 
 
 @router.get('/', status_code=status.HTTP_200_OK)
-def obter_inquilinos(db: Session = Depends(get_db), limit: int = 10, page: int = 1):
-    skip = (page - 1) * limit
-    inquilinos = db.query(models.Inquilino).limit(limit).offset(skip).all()
+def obter_inquilinos(db: Session = Depends(get_db)):
+    inquilinos = gateway.obter_inquilinos(db=db)
 
-    return {'results': len(inquilinos), 'inquilinos': inquilinos}
+    return {'quantidade': len(inquilinos), 'inquilinos': inquilinos}
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def criar_inquilino(payload: schemas.Inquilino, db: Session = Depends(get_db)):
-    novo_inquilino = models.Inquilino(**payload.model_dump())
-    db.add(novo_inquilino)
-    db.commit()
-    db.refresh(novo_inquilino)
+def criar_inquilino(dados: schemas.Inquilino, db: Session = Depends(get_db)):
+    return gateway.criar_inquilino(dados, db)
 
-    return {"inquilino": novo_inquilino}
 
+@router.get('/{inquilino_id}', status_code=status.HTTP_200_OK)
+def obter_um_inquilino(id: int, db: Session = Depends(get_db)):
+    inquilino = gateway.obter_um_inquilino(id, db)
+
+    return {'inquilino': inquilino}
+
+
+@router.patch('/{inquilino_id}', status_code=status.HTTP_200_OK)
+def editar_um_inquilino(id: int, dados: schemas.Inquilino, db: Session = Depends(get_db)):
+    inquilino_atualizado = gateway.editar_um_inquilino(id, dados, db)
+
+    return {'inquilino_atualizado': inquilino_atualizado}
+
+
+@router.delete('/{inquilino_id}', status_code=status.HTTP_204_NO_CONTENT)
+def excluir_um_inquilino(id: int, db: Session = Depends(get_db)):
+    inquilino_id = gateway.excluir_um_inquilino(id, db)
+
+    return {'message': f'Inquilino {inquilino_id} excluído'}
