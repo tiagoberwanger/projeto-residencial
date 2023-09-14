5b1b2529d2845b5fdb17467cd1e18e7859496143
Tiago Berwanger <tiago.berwanger@mercos.com>
2023-09-13 23:15:40 -0300

    checa se um inquilino ou imovel existem antes de criar um contrato

--- app/contratos/crud.py
+++ app/contratos/crud.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 import contratos.gateway as gateway
+import inquilinos.gateway as inquilino_gateway
+import imoveis.gateway as imovel_gateway
 import schemas
 from database import get_db
 
@@ -16,5 +18,13 @@ def obter_contratos(db: Session = Depends(get_db)):
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def criar_contrato(dados: schemas.Contrato, db: Session = Depends(get_db)):
-    return gateway.criar_contrato(dados, db)
+def criar_contrato(contrato: schemas.Contrato, db: Session = Depends(get_db)):
+    inquilino = inquilino_gateway.obter_um_inquilino(contrato.inquilino_id, db)
+    if not inquilino:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Inquilino não encontrado")
+
+    imovel = imovel_gateway.obter_um_imovel(contrato.imovel_id, db)
+    if not imovel:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Imóvel não encontrado")
+
+    return gateway.criar_contrato(contrato, db)
