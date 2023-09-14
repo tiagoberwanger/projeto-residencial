a2fa17772fabaccf04209a8e8950d44653337413
Tiago Berwanger <tiago.berwanger@mercos.com>
2023-09-13 21:45:00 -0300

    ajustes na organização dos models

--- app/models.py
+++ app/models.py
@@ -11,12 +11,14 @@ class Inquilino(Base):
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     nome = Column(String, nullable=False)
-    cpf = Column(Integer, nullable=False)
+    cpf = Column(String(14), nullable=False, unique=True)
     data_nascimento = Column(Date, nullable=True)
     cidade = Column(String, nullable=True)
     estado = Column(String, nullable=True)
     email = Column(String, nullable=True)
-    ultima_alteracao = Column(TIMESTAMP(timezone=True), default=None, onupdate=func.now())
+    ultima_alteracao = Column(TIMESTAMP(timezone=True), default=func.now(), onupdate=func.now())
+
+    contrato = relationship('Contrato', backref='inquilino')
 
 
 class Imovel(Base):
@@ -27,22 +29,25 @@ class Imovel(Base):
     numero = Column(Integer, nullable=False)
     disponivel = Column(Boolean, default=True, nullable=False)
     descricao = Column(String, nullable=True)
-    valor = Column(DECIMAL, nullable=True)
+    valor = Column(DECIMAL(precision=10, scale=2), nullable=True)
     endereco = Column(String, nullable=True)
     area = Column(Integer, nullable=True)
-    ultima_alteracao = Column(TIMESTAMP(timezone=True), default=None, onupdate=func.now())
+    ultima_alteracao = Column(TIMESTAMP(timezone=True), default=func.now(), onupdate=func.now())
+
+    contrato = relationship('Contrato', backref='imovel')
 
 
 class Contrato(Base):
     __tablename__ = "contratos"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    inquilino_id = Column(Integer, ForeignKey("inquilinos.id"), nullable=False)
-    imovel_id = Column(Integer, ForeignKey("imoveis.id"), nullable=False)
-    inquilino = relationship("Inquilino", backref="contratos")
-    imovel = relationship("Imovel", backref="contratos")
     data_inicio = Column(Date, nullable=False)
     data_termino = Column(Date, nullable=False)
-    valor_aluguel = Column(DECIMAL, nullable=False)
+    valor_aluguel = Column(DECIMAL(precision=10, scale=2), nullable=False)
     status = Column(Enum(StatusEnum), nullable=False)
-    ultima_alteracao = Column(TIMESTAMP(timezone=True), default=None, onupdate=func.now())
+    ultima_alteracao = Column(TIMESTAMP(timezone=True), default=func.now(), onupdate=func.now())
+
+    inquilino_id = Column(Integer, ForeignKey("inquilinos.id"), nullable=False)
+    imovel_id = Column(Integer, ForeignKey("imoveis.id"), nullable=False)
+    inquilino = relationship('Inquilino', back_populates='contratos')
+    imovel = relationship('Imovel', back_populates='contratos')
